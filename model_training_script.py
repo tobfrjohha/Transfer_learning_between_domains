c36c5bd344a7fe28aa3447eefbe20f701483ce14
Tobias Frenger <37848545+Tobias-Frenger@users.noreply.github.com>
2020-05-07 10:45:32 +0200

    Update model_training_script.py

--- model_training_script.py
+++ model_training_script.py
@@ -9,7 +9,6 @@ import numpy as np
 import csv
 import os, shutil
 
-
 def choose_ds(name):
     if name == "boats":
         tr_x,tr_y,te_x,te_y = boats_dataset()
@@ -36,33 +35,26 @@ def show(history):
     ax.legend()
 
 datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        zca_epsilon=1e-06,  # epsilon for ZCA whitening
-        rotation_range=20.0,  # randomly rotate images in the range (degrees, 0 to 180)
-        # randomly shift images horizontally (fraction of total width)
-        width_shift_range=0.2,
-        # randomly shift images vertically (fraction of total height)
-        height_shift_range=0.2,
-        shear_range=0.15,  # set range for random shear
-        zoom_range=0.15,  # set range for random zoom
-        channel_shift_range=0.15,  # set range for random channel shifts
-        # set mode for filling points outside the input boundaries
-        fill_mode='nearest',
-        cval=0.,  # value used for fill_mode = "constant"
-        horizontal_flip=True,  # randomly flip images
-        #vertical_flip=True,  # randomly flip images
-        # set rescaling factor (applied before any other transformation)
-        rescale=None,
-        # set function that will be applied on each input
-        preprocessing_function=None,
-        # image data format, either "channels_first" or "channels_last"
-        data_format=None,
-        # fraction of images reserved for validation (strictly between 0 and 1)
-        validation_split=0.0)
+        featurewise_center=False,               # set input mean to 0 over the dataset
+        samplewise_center=False,                # set each sample mean to 0
+        featurewise_std_normalization=False,    # divide inputs by std of the dataset
+        samplewise_std_normalization=False,     # divide each input by its std
+        zca_whitening=False,                    # apply ZCA whitening
+        zca_epsilon=1e-06,                      # epsilon for ZCA whitening
+        rotation_range=20.0,                    # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.2,                  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.2,                 # randomly shift images vertically (fraction of total height)
+        shear_range=0.15,                       # set range for random shear
+        zoom_range=0.15,                        # set range for random zoom
+        channel_shift_range=0.15,               # set range for random channel shifts
+        fill_mode='nearest',                    # set mode for filling points outside the input boundaries
+        cval=0.,                                # value used for fill_mode = "constant"
+        horizontal_flip=True,                   # randomly flip images
+        #vertical_flip=True,                    # randomly flip images
+        rescale=None,                           # set rescaling factor (applied before any other transformation)
+        preprocessing_function=None,            # set function that will be applied on each input
+        data_format=None,                       # image data format, either "channels_first" or "channels_last"
+        validation_split=0.0)                   # fraction of images reserved for validation (strictly between 0 and 1)
 
     # Compute quantities required for feature-wise normalization
     # (std, mean, and principal components if ZCA whitening is applied).
@@ -167,9 +159,6 @@ def train_model(new_model, dataset, iteration, experimentor, experiment_name):
 
     model.fit(train_x, train_y, batch_size=32, validation_split=0.10, epochs = 1000, callbacks=[checkpoint_val_loss, csv_logger, early_stopping])
     
-    # Create the evaluation foldermodel = load_model("/home/johan/Experiment/Tobias_Experiments/Transferlearning_mini_inception_resnet/saved_models/V1_/V1_mini_inception_resnet_45-0.54_best_val_model.hdf5")
-
-
     if (not os.path.exists(saved_folder_name + "/evaluation")):
         os.mkdir(saved_folder_name + "/evaluation")
     
